aefea6d6c67d11f51cb3a883b6956a461f79e257
Troy1010 <tminus1010@gmail.com>
2016-12-20 14:43:47 -0600

    Start

---
+++ CommonCode/CommonCodeClasses.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonCode
+{
+    public class CommonCode
+    {
+        public object GetMatchedObject(string MatchedString, Dictionary<object, string[]> d2dMatchables)
+        {
+            foreach (KeyValuePair<object, string[]> vPair in d2dMatchables)
+            {
+                foreach (string sString in vPair.Value)
+                {
+                    if (sString == MatchedString)
+                    {
+                        return vPair.Key;
+                    }
+                }
+            }
+            return null;
+        }
+        public string GetPatternFromMatchables(Dictionary<object, string[]> d2dMatchables)
+        {
+            bool bDoOnce = false;
+            string sPattern;
+            foreach (KeyValuePair<object, string[]> vPair in d2dMatchables)
+            {
+                foreach (string sString in vPair.Value)
+                {
+                    if (!bDoOnce)
+                    {
+                        bDoOnce = true;
+                        sPattern += "|";
+                    }
+                    sPattern += sString;
+                }
+            }
+            return "\b("+sPattern+")\b";
+        }
+    }
+}
